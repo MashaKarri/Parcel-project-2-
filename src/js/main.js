4b81d15781757fc97bd42c90a6b4fbb64044ac3f
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2025-10-14 19:40:36 +0300

    Parcel-project-2-4

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 import average from "./average";
 import sum from "./sum";
+import stringToArray from './string-to-array.js';
 
 const numbers = [1, 2, 3, 4, 5];
 const avg = average(numbers);
@@ -7,3 +8,9 @@ console.log(`Average: ${avg}`);
 
 const result = sum(numbers);
 console.log(`Sum: ${result}`);
+
+
+const str = 'one,two,three,four,five';
+const separator = ',';
+const result2 = stringToArray(str, separator);
+console.log(`Result: ${result2}`);
---
+++ src/js/string-to-array.js
@@ -0,0 +1,5 @@
+// який містить функцію для перетворення рядка в масив за допомогою певного роздільника.
+
+export default function stringToArray(str, separator) {
+  return str.split(separator);
+}
