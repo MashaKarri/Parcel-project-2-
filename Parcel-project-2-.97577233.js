03fe2d53ff8e03f0ec968b649e0df249861814c0
Mariya Dovgalyuk <162305105+MashaKarri@users.noreply.github.com>
2025-10-14 19:22:53 +0300

    Updates

---
+++ Parcel-project-2-.97577233.js
@@ -0,0 +1,2 @@
+const e=[1,2,3,4,5],o=e.reduce((e,o)=>e+o,0)/e.length;console.log(`Average: ${o}`);const c=e.reduce((e,o)=>e+o,0);console.log(`Sum: ${c}`);
+//# sourceMappingURL=Parcel-project-2-.97577233.js.map
